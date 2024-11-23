cc2fffcc230cae19fd06a5139b4f5adccc906a58
yssongyao <yssongyao@gmail.com>
2024-11-23 14:53:16 +0800

    image folder path

--- src/Components/Main/PhotoGallery.js
+++ src/Components/Main/PhotoGallery.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import news1 from './assets/image/news1.jpg';
-import news2 from './assets/image/news2.jpg';
-import news3 from './assets/image/news3.jpg';
+import news1 from '../../assets/image/news1.jpg';
+import news2 from '../../assets/image/news2.jpg';
+import news3 from '../../assets/image/news3.jpg';
 
 const PhotoGallery = () => {
 
